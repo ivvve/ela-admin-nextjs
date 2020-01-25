1ed12ca58f59b1c70086337010d7a5b185bd370c
devson <sonyc5720@gmail.com>
2020-01-25 18:27:49 +0900

    feature: added AdminCategory enum

---
+++ common/constant/admin-category.ts
@@ -0,0 +1,11 @@
+export enum AdminCategory {
+  Dashboard = 'dashboard',
+  Components = 'components',
+  Tables = 'tables',
+  Forms = 'forms',
+  Icons = 'icons',
+  Widgets = 'widgets',
+  Charts = 'charts',
+  Maps = 'maps',
+  Pages = 'pages'
+}
