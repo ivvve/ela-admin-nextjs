5aff234794ca4584e9f411c6fdaaa059bd859da3
devson <sonyc5720@gmail.com>
2020-01-27 21:31:41 +0900

    feature: added components/grids

---
+++ pages/components/grids.tsx
@@ -0,0 +1,324 @@
+import Layout from "../../components/layout/layout";
+
+export default function Grids() {
+  return (
+    <Layout title='Grids'>
+
+      <h5 className="heading-title mb-1 text-secondary">Fixed Grid</h5>
+      <div className="row">
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+        <div className="col">
+          <section className="card">
+            <div className="card-body text-secondary">.col</div>
+          </section>
+        </div>
+      </div>
+
+
+      <h5 className="heading-title mb-1 mt-4 text-secondary">Desktop Grid</h5>
+      <div className="row">
+        <div className="col col-lg-12">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-12</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-6">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-6</div>
+          </section>
+        </div>
+        <div className="col-lg-6 ">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-6</div>
+          </section>
+        </div>
+      </div>
+
+
+      <div className="row">
+        <div className="col-lg-4">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-4</div>
+          </section>
+        </div>
+        <div className="col-lg-4">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-4</div>
+          </section>
+        </div>
+        <div className="col-lg-4">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-4</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-3">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-3</div>
+          </section>
+        </div>
+        <div className="col-lg-3">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-3</div>
+          </section>
+        </div>
+        <div className="col-lg-3">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-3</div>
+          </section>
+        </div>
+        <div className="col-lg-3">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-3</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-2">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-2</div>
+          </section>
+        </div>
+        <div className="col-lg-2">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-2</div>
+          </section>
+        </div>
+        <div className="col-lg-2">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-2</div>
+          </section>
+        </div>
+        <div className="col-lg-2">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-2</div>
+          </section>
+        </div>
+        <div className="col-lg-2">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-2</div>
+          </section>
+        </div>
+        <div className="col-lg-2">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-2</div>
+          </section>
+        </div>
+      </div>
+
+
+      <h4 className="heading-title mt-5 mb-3 text-secondary">Mobile, Tablet, and Desktop</h4>
+
+      <div className="row">
+        <div className="col-2">
+          <section className="card">
+            <div className="card-body text-secondary">col-2</div>
+          </section>
+        </div>
+        <div className="col-2">
+          <section className="card">
+            <div className="card-body text-secondary">col-2</div>
+          </section>
+        </div>
+        <div className="col-lg-8">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-8</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-3">
+          <section className="card">
+            <div className="card-body text-secondary">col-sm-3</div>
+          </section>
+        </div>
+        <div className="col-sm-3">
+          <section className="card">
+            <div className="card-body text-secondary">col-sm-3</div>
+          </section>
+        </div>
+        <div className="col-lg-6">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-6</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4">
+          <section className="card">
+            <div className="card-body text-secondary">col-md-4</div>
+          </section>
+        </div>
+        <div className="col-md-4">
+          <section className="card">
+            <div className="card-body text-secondary">col-md-4</div>
+          </section>
+        </div>
+        <div className="col-lg-4">
+          <section className="card">
+            <div className="card-body text-secondary">col-lg-4</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-6">
+          <section className="card">
+            <div className="card-body text-secondary">col-sm-6</div>
+          </section>
+        </div>
+        <div className="col-sm-6">
+          <section className="card">
+            <div className="card-body text-secondary">col-sm-6</div>
+          </section>
+        </div>
+      </div>
+
+
+      <h5 className="heading-title mb-1 mt-4 text-secondary">Offset Grid</h5>
+
+      <div className="row">
+        <div className="col-md-6 offset-md-6 col-sm-6 ml-auto">
+          <section className="card">
+            <div className="card-body text-secondary">col-md-6 offset-md-6 col-sm-6</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-6 offset-md-3 mr-auto ml-auto">
+          <section className="card">
+            <div className="card-body text-secondary">.col-md-6 .offset-md-3</div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4">
+          <section className="card">
+            <div className="card-body text-secondary"> .col-md-4</div>
+          </section>
+        </div>
+        <div className="col-md-4 ml-auto">
+          <section className="card">
+            <div className="card-body text-secondary">.col-md-4 .ml-auto</div>
+          </section>
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
